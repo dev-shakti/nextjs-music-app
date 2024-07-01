fa623b6b7b26df801ba5384ede3cdd5a4b73a657
shakti sumant das <shakti.20k@gmail.com>
2024-07-01 23:50:55 +0530

    add spotlight UI component

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <div className="p-4 relative z-10 w-full text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Master The Music Art
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive music courses and transform your musical
+          journey today. Whether you're a beginner or looking to refine your
+          skills, join us to unlock your true potential.
+        </p>
+        <div className="mt-4">
+          <Link href={"/courses"}>Explore Coureses</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "./components/HeroSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.2]">
-      <h1 className="text-center text-2xl">Music App</h1>
+      <HeroSection/>
     </main>
   );
 }
