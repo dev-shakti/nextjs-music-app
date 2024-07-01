5ea9e1c4366f19b76f3dc8ead57366b9b520f397
shakti sumant das <shakti.20k@gmail.com>
2024-07-02 00:07:04 +0530

    add spotlight effect in hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import React from "react";
+import { Spotlight } from "./ui/Spotlight"
 
 const HeroSection = () => {
   return (
     <section className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="blue"
+      />
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master The Music Art
         </h1>
