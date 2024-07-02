ab32bb2b34e74654a95ec4e1793c7dc84f29586f
shakti sumant das <shakti.20k@gmail.com>
2024-07-02 13:58:11 +0530

    hero component done

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import React from "react";
-import { Spotlight } from "./ui/Spotlight"
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
     <section className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="blue"
-      />
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="white"
+        />
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master The Music Art
         </h1>
@@ -19,7 +20,14 @@ const HeroSection = () => {
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Link href={"/courses"}>Explore Coureses</Link>
+          <Link href={"/courses"}>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore Coureses
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
