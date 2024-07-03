1c0552dfeb0c7e548a491f96027bb6987f77f3ba
shakti sumant das <shakti.20k@gmail.com>
2024-07-03 14:53:11 +0530

    add instructors components and configure next.config.mjs file

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -37,8 +37,8 @@ const musicSchoolTestimonials = [
 
 const Testimonials = () => {
   return (
-    <div className='h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden'>
-       <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
+    <div className='h-[40rem] w-full testimonials-bg relative flex flex-col items-center justify-center overflow-hidden'>
+       <h2 className="text-3xl font-bold text-center mb-8 z-10 text-white">Hear our Harmony: Voices of success</h2>
        <div>
        <InfiniteMovingCards
         items={musicSchoolTestimonials}
