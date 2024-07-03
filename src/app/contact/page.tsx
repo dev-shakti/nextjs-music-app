7aa2fea7d27d6d84179cf4bb428293f2a4849d17
shakti sumant das <shakti.20k@gmail.com>
2024-07-03 15:43:26 +0530

    Configure Tailwind and implement meteor effect on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { Meteors } from "../components/ui/meteors";
 const ContactPage = () => {
   return (
-    <div>
-      Contact us
+    <div className='min-h-screen bg-black py-12 pt-36'>
+       <Meteors number={20} />
     </div>
   )
 }
